74079696d0659d69351a8eebf013adb1c25f8038
Barbssx <bsthefaani@gmail.com>
2025-04-09 13:57:55 -0300

    deploy automático config

--- vue.config.js
+++ vue.config.js
@@ -7,6 +7,7 @@ module.exports = defineConfig({
     plugins: [
       new webpack.ProvidePlugin({
         process: 'process/browser',
+        publicPath: '/dashboard/'
       }),
     ],
     resolve: {
