84f3f728172b1b25c714fda0eec491f09d56fa56
Barbssx <bsthefaani@gmail.com>
2025-05-23 21:41:59 -0300

    Mais uma tentativa de arrumar a conexão entre back e front, socorro Deus

--- backend/server.js
+++ backend/server.js
@@ -5,24 +5,26 @@ const uploadRoutes = require('./routes/upload');
 const registrosRoutes = require('./routes/registros');
 
 const allowedOrigins = [
-  'http://localhost:5173',
-  'https://dashboard-eight-xi-35.vercel.app/',
-  'https://dashboard-production-4d11e.up.railway.app'
+  'https://dashboard-eight-xi-35.vercel.app',
+  'https://horta-inteligente-jxenulci8-barbaras-projects-04d294a6.vercel.app',
+  'http://localhost:8080' 
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Origem não permitida pelo CORS'));
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = `O CORS bloqueou a origem: ${origin}`;
+      return callback(new Error(msg), false);
     }
+    return callback(null, true);
   }
 }));
 
 app.use(express.json());
 app.use('/api/upload', uploadRoutes);
-app.use('/api/registros', registrosRoutes); 
+app.use('/api/registros', registrosRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
