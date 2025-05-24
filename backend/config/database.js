bf9ca7e2d7afa120b04a01fce63c5e4d828b780e
Barbssx <bsthefaani@gmail.com>
2025-05-23 21:13:03 -0300

    Instalando mais pacotes para inicialização do backend na nuvem

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
+  protocol: 'postgres',
+  logging: false,
   dialectOptions: {
     ssl: {
+      require: true,
       rejectUnauthorized: false,
     },
   },
