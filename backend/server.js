5b89941be07db35acb618796b91b93b339e0142d
Barbssx <bsthefaani@gmail.com>
2025-05-23 22:26:39 -0300

    Tinha esquecido de automatizar a criação da pasta para upload dos arquivos TURURU

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,30 @@
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
+
 const app = express();
+
+const uploadsDir = path.join(__dirname, 'uploads');
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir);
+  console.log('Pasta uploads criada.');
+}
+
 const uploadRoutes = require('./routes/upload');
 const registrosRoutes = require('./routes/registros');
 
 const allowedOrigins = [
   'https://dashboard-eight-xi-35.vercel.app',
   'https://horta-inteligente-jxenulci8-barbaras-projects-04d294a6.vercel.app',
-  'http://localhost:8080' 
+  'http://localhost:8080'
 ];
 
 app.use(cors({
-  origin: function(origin, callback){
-    if(!origin) return callback(null, true);
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true);
 
-    if(allowedOrigins.indexOf(origin) === -1){
+    if (allowedOrigins.indexOf(origin) === -1) {
       const msg = `O CORS bloqueou a origem: ${origin}`;
       return callback(new Error(msg), false);
     }
@@ -23,6 +33,7 @@ app.use(cors({
 }));
 
 app.use(express.json());
+
 app.use('/api/upload', uploadRoutes);
 app.use('/api/registros', registrosRoutes);
 
